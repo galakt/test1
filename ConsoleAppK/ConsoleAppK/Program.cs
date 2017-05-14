e40120f110c91be228dfe96f73d1d5a62db05802
galakt <gala-vadim@narod.ru>
2017-05-14 18:26:52 +0300

    Add wcf startup

--- ConsoleAppK/ConsoleAppK/Program.cs
+++ ConsoleAppK/ConsoleAppK/Program.cs
@@ -6,6 +6,12 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Owin.Hosting;
 using System.Runtime.CompilerServices;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using ConsoleAppK.Data;
+using ConsoleAppK.DI.Wcf;
+using ConsoleAppK.WCF;
+using Microsoft.Practices.Unity;
 
 [assembly: InternalsVisibleTo("ConsoleAppK.Tests")]
 
@@ -19,9 +25,34 @@ namespace ConsoleAppK
         {
             // Start OWIN host 
             WebApp.Start<Startup>(url: BaseAddress);
-            
+
+            // Start WCF
+            try
+            {
+                var host = CreateServiceHost();
+                host.Open();
+            }
+            catch (AddressAccessDeniedException ae)
+            {
+                Console.WriteLine(ae);
+                Console.WriteLine($"Try to netsh http add urlacl url= user=" +
+                                  $"or run under Administrator");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
             Console.WriteLine("Started");
             Console.ReadLine();
+        }
+
+        internal static ServiceHost CreateServiceHost()
+        {
+            var host = new UnityServiceHost(typeof(UserInfoProvider), new Uri(BaseAddress));
+            host.Container.RegisterType<IUserInfoRepository, UserInfoRepository>(new ContainerControlledLifetimeManager());
+
+            return host;
         }
     }
 }
