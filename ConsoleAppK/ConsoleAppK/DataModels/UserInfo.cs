69ec919e6e9d34f6d80edd36c7e038284a52342a
galakt <gala-vadim@narod.ru>
2017-05-14 18:27:27 +0300

    Add data class

---
+++ ConsoleAppK/ConsoleAppK/DataModels/UserInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppK.DataModels
+{
+    [DataContract]
+    public class UserInfo
+    {
+        [DataMember]
+        public Guid UserId { get; set; }
+
+        [DataMember]
+        public bool? AdvertisingOptIn { get; set; }
+
+        [DataMember]
+        public string CountryIsoCode { get; set; }
+
+        [DataMember]
+        public DateTime DateModified { get; set; }
+
+        [DataMember]
+        public string Locale { get; set; }
+    }
+}
